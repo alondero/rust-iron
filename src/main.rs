ad518466883aea2c75b5e205633e410b532239c6
Adam Londero <alondero@gmail.com>
2016-09-29 22:06:23 +0100

    Tidying up the module usages

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,14 @@ extern crate router;
 
 use iron::prelude::*;
 use router::Router;
+use controllers::friendly_controller;
 
 mod controllers;
 
 fn main() {
     let mut router = Router::new();
 
-    router.get("/", controllers::friendly_controller::friendly_greeting, "message");
+    router.get("/", friendly_controller::friendly_greeting, "message");
 
     Iron::new(router).http("localhost:3000").unwrap();
 }
